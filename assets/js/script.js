ae0af25c735f0400ed04569c55bce66922706e7e
gaaiuss <caio.gui.castro@gmail.com>
2025-09-23 10:16:07 -0300

    get directory

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
-const textElement = document.getElementById('text');
+async function openDirectoryPicker() {
+  try {
+    const directoryHandle = await window.showDirectoryPicker();
+    console.log('Directory selected:', directoryHandle.name);
+    // You can then use the directoryHandle to access files within the selected directory.
+  } catch (error) {
+    console.error('Error opening directory picker:', error);
+  }
+}
 
-textElement.innerHTML = '<p>Files Selected: ...<p/>';
+document.addEventListener('click', e => {
+    const lmt = e.target;
+    if (lmt.classList.contains('select-bt')) openDirectoryPicker();
+});
