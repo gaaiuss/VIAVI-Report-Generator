2f2e5f8bf6b0497779c0c0f672d3b1de1c323dda
gaaiuss <caio.gui.castro@gmail.com>
2025-09-29 15:21:13 -0300

    get files

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,31 +6,18 @@ async function uploadFiles() {
     alert("Please select at least one JSON file.");
     return;
   }
+  readFiles(files)
+}
 
-  const formData = new FormData();
-  for (let file of files) {
-    formData.append("files", file); // "files" must match your backend field name
-  }
-
-  try {
-    const response = await fetch("http://127.0.0.1:8000/upload", { // your backend URL here
-      method: "POST",
-      body: formData
-    });
-
-    if (response.ok) {
-      const result = await response.json();
-      alert("Files uploaded successfully: " + JSON.stringify(result));
-    } else {
-      alert("Upload failed: " + response.statusText);
-    }
-  } catch (err) {
-    console.error(err);
-    alert("Error uploading files.");
+function readFiles(files) {
+  const fs = require('fs')
+  for (file of files) {
+    const data = fs.readFileSync(file.name, 'utf8');
+    console.log(data);
   }
 }
 
 document.addEventListener('click', e => {
   const lmt = e.target;
-  if (lmt.classList.contains('upl-btn')) uploadFiles();
+  if (lmt.classList.contains('gen-btn')) uploadFiles();
 });
